81e975e835fccd2418bf2f98256c3492ea32eec1
LakshmiPrabha <67004947+lakshmiprabhat@users.noreply.github.com>
2021-03-17 01:07:09 +0530

    commit changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable linebreak-style */
 import React from 'react';
-import './App.css';
+import HomePage from './components/HomePage/HomePage';
+import GamePage from './components/GamePage/GamePage';
+import Route from './components/common/Route';
+import './components/HomePage/HomePage.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fast Fingers </h1>
+    <div>
+      <Route path="/">
+        <HomePage />
+      </Route>
+      <Route path="/game-page">
+        <GamePage />
+      </Route>
     </div>
   );
 }
