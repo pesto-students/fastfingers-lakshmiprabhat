91b098eab4220edfef1443f297bbf77d2a8b7d7d
LakshmiPrabha <67004947+lakshmiprabhat@users.noreply.github.com>
2021-03-25 00:12:38 +0530

    modify color change functionality on randomword

--- src/components/GamePage/WordPage.jsx
+++ src/components/GamePage/WordPage.jsx
@@ -1,79 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import Timer from './Timer';
+import RandomWord from './RandomWord';
+import { getRandomWord ,getDataFromLocalStorage} from '../common/Util';
 import ProtoTypes from "prop-types";
-import "./GamePage.scss";
-import Timer from "./Timer";
-import data from "../common/data/dictionary.json";
-import {getDataFromLocalStorage} from "../common/Util";
-import SetColorsOnRandomWord from "./SetColorsOnRandomWord";
+import './GamePage.scss';
 
-export default function WordPage({ handleStopGame }) {
-const [randomWord, setRandomWord] = useState('');
-const [playerInput, setPlayerInput] = useState('');
-const difficultyLevel = Number(getDataFromLocalStorage("difficultyLevel"));
-const [currentDifficultyLevel, setCurrentDifficultyLevel] = useState(
-    difficultyLevel
-    );
-const [timerValue, setTimerValue] = useState(-1);
-const playerInputRef = React.createRef();
+export default function WordPage({ handleStopGame }){
+    const [difficultyLevel, setDifficultyLevel] = useState(Number(getDataFromLocalStorage('difficultyLevel')));
+    const [randomWord, setRandomWord] = useState('');    
+    const [maxTime, setMaxTime] = useState(-1);
+    const [inputWord, setInputWord] = useState('');
+    const inputWordRef = React.createRef();
 
-const getRandomWord = () => {
-    let filteredWords = [];
-    if (currentDifficultyLevel < 1.5) {
-        filteredWords = data.filter((word) => word.length <= 4);
-    } else if (currentDifficultyLevel >= 1.5 && currentDifficultyLevel < 2) {
-        filteredWords = data.filter(
-        (word) => word.length >= 5 && word.length <= 8
-        );
-    } else {
-        filteredWords = data.filter((word) => word.length > 8);
+    const handleInputWordChange = (e) => {
+        setInputWord(e.target.value);
+        if (randomWord.toLowerCase() === e.target.value.toLowerCase()) {
+            setInputWord('');           
+            setDifficultyLevel(difficultyLevel+0.01);
+            setTargetWord();            
+        }
     }
-    const randomIndex = Math.floor(Math.random() * filteredWords.length);
-    const newrandomWord = filteredWords[randomIndex];
-    setRandomWord(newrandomWord);
-    setPlayerInput("");
-    let newTimerValue =
-        Math.floor(newrandomWord.length / currentDifficultyLevel) * 1000;
-    if (newTimerValue > 2000) setTimerValue(newTimerValue);
-    else setTimerValue(2000);
-    };
 
-    const handleTextChange = (event) => {
-    setPlayerInput(event.target.value);
-    if (randomWord.toLowerCase() === event.target.value.toLowerCase()) {
-        getRandomWord();
-        setCurrentDifficultyLevel(currentDifficultyLevel + 0.01);
+    const setTargetWord = () => {
+        const randomWord = getRandomWord(difficultyLevel);
+        const maxTime = Math.ceil(randomWord.length / difficultyLevel) * 1000;
+        setRandomWord(randomWord);
+        setMaxTime(maxTime > 2000 ? maxTime : 2000);
     }
-    };
 
-useEffect(() => {
-    getRandomWord();
-    if (playerInputRef.current){
-        playerInputRef.current.focus();
-    } // eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
-
-return (
-    <div className="playArea">
-    <Timer
-            timerValue={timerValue}
-            handleStopGame={handleStopGame}
-        ></Timer>            
-        <div className="randomWord">
-        <SetColorsOnRandomWord randomWord={randomWord} playerInput={playerInput}/>  
+    useEffect(() => {
+        setTargetWord();
+        if (inputWordRef.current) {
+            inputWordRef.current.focus();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+    
+    return(
+        <div className="playArea">
+            <Timer timerValue={maxTime} key={randomWord} handleStopGame={handleStopGame}/>
+            <div className="randomWord">
+            <RandomWord randomWord={randomWord} inputWord={inputWord}/>
+            </div>
+            <div className="inputWordArea">
+            <input type="text" className="playerInput" value={inputWord} onChange={handleInputWordChange} ref={inputWordRef}/>
+            </div>
         </div>
-        <div className="inputWordArea">
-            <input
-            type="text"
-            className="playerInput"
-            value={playerInput}
-            onChange={handleTextChange}
-            ref={playerInputRef}
-            />
-        </div>
-        </div>
-);
+    )
 }
 
 WordPage.protoTypes = {
-    handleStopGame: ProtoTypes.func.isRequired
+    handleStopGame : ProtoTypes.func.isRequired
 }
--- src/components/common/Util.jsx
+++ src/components/common/Util.jsx
@@ -1,3 +1,5 @@
+import WordsCollection from "./data/dictionary.json";
+
 export const saveDataToLocalStorage = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 export const getDataFromLocalStorage = (key) =>
@@ -7,24 +9,56 @@ export const levels = [
   { level: 1.5, levelDesc: "MEDIUM" },
   { level: 2, levelDesc: "HARD" }
 ];
-export function formatTimeLeft(time,format ="ss:ms") {
+export function formatTimeLeft(time, format = "ss:ms") {
   let minutes = Math.floor(time / 1000 / 60);
   let seconds = Math.floor(time / 1000);
   let miliseconds = (time % 1000) / 10;
 
-  if(seconds % 60 === 0){
+  if (seconds % 60 === 0) {
     seconds = 0;
   }
-  if(seconds > 60) {
+  if (seconds > 60) {
     seconds = Math.floor(seconds % 60);
   }
 
   if (format === "mm:ss") {
-    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds
-      }`;
-  }
-  else {
-    return `${seconds < 10 ? `0${seconds}` : seconds}:${miliseconds < 10 ? `0${miliseconds}` : miliseconds
-      }`;
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${
+      seconds < 10 ? `0${seconds}` : seconds
+    }`;
+  } else {
+    return `${seconds < 10 ? `0${seconds}` : seconds}:${
+      miliseconds < 10 ? `0${miliseconds}` : miliseconds
+    }`;
   }
 }
+
+const FULL_DASH_ARRAY = 283;
+
+export function calculateTimeFraction(timeLeft, maxTime) {
+  const rawTimeFraction = timeLeft / maxTime;
+  return (
+    (rawTimeFraction - (1 / maxTime) * (1 - rawTimeFraction)) * FULL_DASH_ARRAY
+  );
+}
+
+const EASY_DIFFICULTY_WORDARR = WordsCollection.filter(
+  (word) => word.length <= 4
+);
+const MEDIUM_DIFFICULTY_WORDARR = WordsCollection.filter(
+  (word) => word.length >= 5 && word.length <= 8
+);
+const HARD_DIFFICULTY_WORDARR = WordsCollection.filter(
+  (word) => word.length > 8
+);
+
+const getRandomIndex = (array) => Math.floor(Math.random() * array.length);
+
+export const getRandomWord = (difficultyLevel) => {
+  if (difficultyLevel < 1.5) {
+    return EASY_DIFFICULTY_WORDARR[getRandomIndex(EASY_DIFFICULTY_WORDARR)];
+  } else if (difficultyLevel > 1.5 && difficultyLevel < 2) {
+    return MEDIUM_DIFFICULTY_WORDARR[getRandomIndex(MEDIUM_DIFFICULTY_WORDARR)];
+  } else {
+    return HARD_DIFFICULTY_WORDARR[getRandomIndex(HARD_DIFFICULTY_WORDARR)];
+  }
+};
